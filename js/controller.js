0b86ef59be3dc0b70abe0a18020c73e5709aed0c
Carles <cespuess@gmail.com>
2024-11-24 13:44:23 +0100

    sombres en triangles min

--- js/controller.js
+++ js/controller.js
@@ -1,91 +1,99 @@
-import { calculateOptionsSeparatorHeight, detectTriangleSelected, trianglesMax, trianglesMin } from "./model.js";
 import {
-    abogadosHover,
-    economistasHover,
-    maximizeTriangles,
-    minimizeTriangles,
-    optionsClassHoverRemove,
-    returnHover, returnNotHover,
-    triangleAndorraHover,
-    triangleMadridHover
+  calculateOptionsSeparatorHeight,
+  detectTriangleSelected,
+  trianglesMax,
+  trianglesMin,
+} from "./model.js";
+import {
+  abogadosHover,
+  economistasHover,
+  maximizeTriangles,
+  minimizeTriangles,
+  optionsClassHoverRemove,
+  returnHover,
+  returnNotHover,
+  triangleAndorraHover,
+  triangleMadridHover,
 } from "./view.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector(".container")
-    const buttonCardAndorra = document.querySelector("#button__card__andorra");
-    const buttonCardMadrid = document.querySelector("#button__card__madrid");
-    const triangleMadridReturn = document.querySelector(".triangle__madrid--return");
+  const container = document.querySelector(".container");
+  const buttonCardAndorra = document.querySelector("#button__card__andorra");
+  const buttonCardMadrid = document.querySelector("#button__card__madrid");
+  const triangleMadridReturn = document.querySelector(
+    ".triangle__madrid--return"
+  );
 
+  // Saber si només es veuen els dos triangles grans
+  let landingInitial = true;
 
+  let triangleHover;
+  let optionHover;
 
-    // Saber si només es veuen els dos triangles grans
-    let landingInitial = true;
-    
-    let triangleHover;
-    let optionHover; 
+  container.addEventListener("mousemove", (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const optionsSeparator = calculateOptionsSeparatorHeight(optionHover);
 
-    container.addEventListener("mousemove", (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-        const optionsSeparator = calculateOptionsSeparatorHeight(optionHover);
-        
-        // Donem les mesures dels triangles grans o petits depenent en quin estat es troba la landing
-        let triangles = landingInitial? trianglesMax: trianglesMin;
+    // Donem les mesures dels triangles grans o petits depenent en quin estat es troba la landing
+    let triangles = landingInitial ? trianglesMax : trianglesMin;
 
-        triangleHover = detectTriangleSelected(mouseX, mouseY, triangles, landingInitial);
-        
-        if (landingInitial) {
-            if (triangleHover === 1) triangleMadridHover();
-            if (triangleHover === 2) triangleAndorraHover();
-        }
+    triangleHover = detectTriangleSelected(
+      mouseX,
+      mouseY,
+      triangles,
+      landingInitial
+    );
 
+    if (landingInitial) {
+      if (triangleHover === 1) triangleMadridHover();
+      if (triangleHover === 2) triangleAndorraHover();
+    }
 
-        // que el l'imatge del return torni al seu tamany normal quan no està hover
-        returnNotHover();
+    // que el l'imatge del return torni al seu tamany normal quan no està hover
+    returnNotHover();
 
-        if (!landingInitial && !triangleHover) {
-            if (mouseY < optionsSeparator) {
-                abogadosHover();
-                optionHover = 'abogados';
-            } else {
-                economistasHover();
-                optionHover = 'economistas';
-            }
-        } else if (!landingInitial && triangleHover === 1) {
-            returnHover();
-            optionHover = undefined;
-            optionsClassHoverRemove();
+    if (!landingInitial && !triangleHover) {
+      if (mouseY < optionsSeparator) {
+        abogadosHover();
+        optionHover = "abogados";
+      } else {
+        economistasHover();
+        optionHover = "economistas";
+      }
+    } else if (!landingInitial && triangleHover === 1) {
+      returnHover();
+      optionHover = undefined;
+      optionsClassHoverRemove();
+    } else {
+      optionHover = undefined;
+      optionsClassHoverRemove();
+    }
+  });
 
-        } else {
-            optionHover = undefined;
-            optionsClassHoverRemove();
-        }
+  triangleMadridReturn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (!landingInitial && triangleHover === 1) {
+      maximizeTriangles();
+      landingInitial = true;
+    }
+    // if (triangleHover === 2) {
+    //     if (landingInitial) {
+    //         minimizeTriangles();
+    //         landingInitial = false;
+    //     }
+    // }
+  });
 
-    })
-    
-    triangleMadridReturn.addEventListener("click", (e) => {
-        e.stopPropagation();
-        if (!landingInitial && triangleHover === 1) {
-                maximizeTriangles();
-                landingInitial = true;
-            }
-        // if (triangleHover === 2) {
-        //     if (landingInitial) {
-        //         minimizeTriangles();
-        //         landingInitial = false;
-        //     } 
-        // }
-    })
+  buttonCardAndorra.addEventListener("click", (e) => {
+    e.stopPropagation();
+    minimizeTriangles();
+    landingInitial = false;
+  });
 
-    buttonCardAndorra.addEventListener("click", (e) => {
-        e.stopPropagation();
-        minimizeTriangles();
-        landingInitial = false;
-    });
-
-    buttonCardMadrid.addEventListener("click", (e) => {
-        e.stopPropagation();
-        window.location.href = "https://www.linkedin.com/company/mesastrigo-morales-arce/?originalSubdomain=es"
-    })
-
-})
+  buttonCardMadrid.addEventListener("click", (e) => {
+    e.stopPropagation();
+    window.location.href =
+      "https://www.linkedin.com/company/mesastrigo-morales-arce/?originalSubdomain=es";
+  });
+});
--- js/view.js
+++ js/view.js
@@ -1,38 +1,42 @@
-    const triangleMadrid = document.querySelector(".triangle__madrid");
-    const triangleAndorra = document.querySelector(".triangle__andorra");
-    const returnImageContainer = document.querySelector(".triangle__madrid--return");
-    const abogados = document.querySelector(".option__abogados");
-    const economistas = document.querySelector(".option__economistas");
-    // const abogadosLink = document.querySelector(".option__abogados--link");
-    // const economistasLink = document.querySelector(".option__economistas--link");
-
+const triangleMadrid = document.querySelector(".triangle__container__madrid");
+const triangleAndorra = document.querySelector(".triangle__container__andorra");
+const triangleAndorraVertical = document.querySelector(".triangle__andorra");
+const returnImageContainer = document.querySelector(
+  ".triangle__madrid--return"
+);
+const abogados = document.querySelector(".option__abogados");
+const economistas = document.querySelector(".option__economistas");
+// const abogadosLink = document.querySelector(".option__abogados--link");
+// const economistasLink = document.querySelector(".option__economistas--link");
 
 export function triangleMadridHover() {
-  triangleAndorra.classList.remove('triangle__madrid--hover');
-  triangleMadrid.classList.add('triangle__madrid--hover');
+  triangleAndorra.classList.remove("triangle__madrid--hover");
+  triangleMadrid.classList.add("triangle__madrid--hover");
 }
 
 export function triangleAndorraHover() {
-  triangleMadrid.classList.remove('triangle__madrid--hover');
-  triangleAndorra.classList.add('triangle__madrid--hover');
+  triangleMadrid.classList.remove("triangle__madrid--hover");
+  triangleAndorra.classList.add("triangle__madrid--hover");
 }
 
 export function minimizeTriangles() {
-  triangleMadrid.classList.add('triangle__madrid--min');
-  triangleAndorra.classList.add('triangle__andorra--vertical');
+  triangleMadrid.classList.add("triangle__madrid--min");
+  triangleAndorra.classList.add("triangle__andorra--min");
+  triangleAndorraVertical.classList.add("triangle__andorra--vertical");
 }
 
 export function maximizeTriangles() {
-  triangleMadrid.classList.remove('triangle__madrid--min');
-  triangleAndorra.classList.remove('triangle__andorra--vertical');
+  triangleMadrid.classList.remove("triangle__madrid--min");
+  triangleAndorra.classList.remove("triangle__andorra--min");
+  triangleAndorraVertical.classList.remove("triangle__andorra--vertical");
 }
 
 export function returnHover() {
-  returnImageContainer.classList.add('triangle__madrid--return--hover')
+  returnImageContainer.classList.add("triangle__madrid--return--hover");
 }
 
 export function returnNotHover() {
-    returnImageContainer.classList.remove('triangle__madrid--return--hover')
+  returnImageContainer.classList.remove("triangle__madrid--return--hover");
 }
 
 export function abogadosHover() {
@@ -43,7 +47,6 @@ export function abogadosHover() {
   abogados.classList.add("option__abogados--hover");
   economistas.classList.add("option__economistas--not-hover");
   // abogadosLink.classList.add("show-link");
-  
 }
 
 export function economistasHover() {
