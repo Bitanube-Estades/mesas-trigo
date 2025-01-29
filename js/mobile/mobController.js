27d5e07ad5a14bd10aad86028f2bafdab90d745b
Carles <cespuess@gmail.com>
2025-01-29 17:23:46 +0100

    transicions a les opcions

--- js/mobile/mobController.js
+++ js/mobile/mobController.js
@@ -1,6 +1,10 @@
 import {heightMainContainer} from "./mobModel.js";
 import {languagesOptions, globe, onClickLanguagesOptions, onClickGlobe} from "../language.js";
-import {sloganIteration} from "./mobView.js";
+//import {sloganIteration} from "./mobView.js";
+import {introBoxes,returnArrow, showMobOptions} from "./mobView.js";
+
+const mainContainer = $(".mob__main__container");
+
 
 // actualitzem la alçada al mainContainer descomptan l'alçada del footer
 heightMainContainer();
@@ -12,6 +16,24 @@ languagesOptions.on("click", onClickLanguagesOptions);
 // per mostrar les opcions d'idiomes
 globe.on("click", onClickGlobe);
 
+// al clickar els logos inicials
+introBoxes.each(function(){
+    $(this).on("click", function(e){
+        e.stopPropagation();
+        const dataCity = $(this).data("city");
+        showMobOptions(`show-mob-${dataCity}`);
+    });
+})
+
+// acció botó fletxa return
+returnArrow.each(function(){
+    $(this).on("click", function(e){
+        e.stopPropagation();
+        const cityElement = $(this).closest('.mob__option');
+        const dataCity = cityElement.data("city");
+        showMobOptions(`show-mob-${dataCity}`);
+    })
+})
 
 //             ------------ Prova faceIn Slogan ---------------------------
 // inicialitzem el faceIn del slogan
--- js/mobile/mobView.js
+++ js/mobile/mobView.js
@@ -1,27 +1,39 @@
-const slogan = $("#mob__slogan h1");
+// const slogan = $(".mob__slogan h1");
+const mainContainer = $(".mob__main__container");
+export const introBoxes = $(".intro__box");
+export const returnArrow = $(".mob__arrow");
 
-
-
-export function sloganIteration() {
-    // Frases slogan
-    const frases = [
-        "Your doubts are our challenges",
-        "Tu cabinete de confianza",
-        "Te asesoramos en Derecho constitucional",
-        "Tanto en Andorra como en Madrid"
-    ];
-
-    let index = 0;
-
-//
-
-// Funció per cambiar el texte
-    function updateText() {
-        slogan.text(frases[index]);
-        index = (index + 1) % frases.length;
-    }
-    slogan.on("animationiteration", updateText);
-
-    updateText()
+export function showMobOptions(className) {
+    mainContainer.toggleClass(className);
 }
+
+
+
+
+// introBoxes.each(()=>{
+//     const dataCity = $(this).data("city");
+//     console.log(dataCity)
+// })
+// export function sloganIteration() {
+//     // Frases slogan
+//     const frases = [
+//         "Your doubts are our challenges",
+//         "Tu cabinete de confianza",
+//         "Te asesoramos en Derecho constitucional",
+//         "Tanto en Andorra como en Madrid"
+//     ];
+//
+//     let index = 0;
+//
+// //
+//
+// // Funció per cambiar el texte
+//     function updateText() {
+//         slogan.text(frases[index]);
+//         index = (index + 1) % frases.length;
+//     }
+//     slogan.on("animationiteration", updateText);
+//
+//     updateText()
+// }
 
